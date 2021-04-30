a06b085c86ac22abfc79d78f53f3197eabe860e7
Pip Cet <pipcet@gmail.com>
2021-04-30 11:47:48 +0000

    oops, commit changes

--- src/adtdump.c
+++ src/adtdump.c
@@ -2,18 +2,20 @@
 #include <sys/fcntl.h>
 #include <sys/mman.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-  int fd = open("/sys/firmware/devicetree/base/reserved-memory/adt/reg");
+  int fd = open("/sys/firmware/devicetree/base/reserved-memory/adt/reg",
+		O_RDONLY);
   uint64_t reg[2];
   if (fd < 0 || read(fd, reg, 16) != 16)
     goto error;
   reg[0] = __builtin_bswap64(reg[0]);
   reg[1] = __builtin_bswap64(reg[1]);
   close(fd);
-  int fd = open("/dev/mem", O_RDONLY);
+  fd = open("/dev/mem", O_RDONLY);
   if (fd < 0)
     goto error;
   void *buf = mmap(NULL, reg[1], PROT_READ, MAP_SHARED, fd, reg[0]);
@@ -25,7 +27,7 @@ int main(int argc, char **argv)
   volatile unsigned *p = buf;
   volatile unsigned *p2 = buf2;
   volatile unsigned *p3 = buf2;
-  for (unsigned long off = 0; off < count; off += 4) {
+  for (unsigned long off = 0; off < reg[1]; off += 4) {
     *p2++ = *p++;
     if (p2 - p3 >= 0x4000)
       p3 += write(1, p3, p2 - p3);
