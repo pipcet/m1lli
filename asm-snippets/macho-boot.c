8afcdaaedd72f0549bc96de695b0ad147fadf890
Pip Cet <pipcet@gmail.com>
2021-05-01 00:53:16 +0000

    cosmetic changes

--- asm-snippets/macho-boot.c
+++ asm-snippets/macho-boot.c
@@ -24,32 +24,32 @@ extern inline void *memset(void *p, int c, size_t size);
 #define MACHO_COMMAND_UNIX_THREAD 0x05
 #define MACHO_COMMAND_SEGMENT_64  0x19
 struct macho_header {
-    u32 irrelevant[5];
-    u32 cmdsize;
-    u32 irrelevant2[2];
-};
+  u32 irrelevant[5];
+  u32 cmdsize;
+  u32 irrelevant2[2];
+} __attribute__((packed));
 
 struct macho_command {
-    u32 type;
-    u32 size;
-    union {
-        struct {
-            u32 thread_type;
-            u32 length;
-            u64 regs[32];
-            u64 pc;
-            u64 regs2[1];
-        } unix_thread;
-        struct {
-            char segname[16];
-            u64 vmaddr;
-            u64 vmsize;
-            u64 fileoff;
-            u64 filesize;
-            u64 unused2[2];
-        } segment_64;
-    } u;
-};
+  u32 type;
+  u32 size;
+  union {
+    struct {
+      u32 thread_type;
+      u32 length;
+      u64 regs[32];
+      u64 pc;
+      u64 regs2[1];
+    } unix_thread;
+    struct {
+      char segname[16];
+      u64 vmaddr;
+      u64 vmsize;
+      u64 fileoff;
+      u64 filesize;
+      u64 unused2[2];
+    } segment_64;
+  } u;
+} __attribute__((packed));
 
 void boot_macho_init(void)
 {
