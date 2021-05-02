257ea0e94e43a72fdb49640adab7c652a6435b6b
Pip Cet <pipcet@gmail.com>
2021-05-02 20:25:13 +0000

    oops, that wasn't the right version

--- src/props-to-fdt.pl
+++ src/props-to-fdt.pl
@@ -2,29 +2,63 @@ my %fdt;
 
 while (<>) {
     chomp;
-    /^(.*?)\.([^. ]*) = (.*)$/ or die;
-    $fdt{$1}{$2} = $3;
+    if (/^(.*?)([^. ]*) = (.*)$/) {
+	push @fdt, $1 unless exists $fdt{$1};
+	push @{$fdt{$1}}, [$2, $3];
+    } elsif (/^(.*?)([^. ]*)$/) {
+	push @fdt, $1 unless exists $fdt{$1};
+	push @{$fdt{$1}}, [$2];
+    }
 }
 
 print "/dts-v1/;\n";
 print "/ {\n";
 my @lastpath;
-for my $node (sort keys %fdt) {
+my $node = "";
+for my $propval (@{$fdt{$node}}) {
+    my ($prop, $val) = @$propval;
+    if (defined $val) {
+	print(("    " x (@lastpath + 1)) . "$prop = $val;\n");
+    } else {
+	print(("    " x (@lastpath + 1)) . "$prop;\n");
+    }
+}
+while (@fdt) {
+    my $node = shift @fdt;
     my @path = split /\./, $node;
     if ($path[0] eq "") { @path = (); }
     while (@lastpath && $lastpath[$#lastpath] ne $path[$#lastpath]) {
-	print "};\n";
 	pop @lastpath;
+	print(("    " x (@lastpath + 1)) . "};\n");
     }
     while (@path > @lastpath) {
-	print "$path[@lastpath] {\n";
+	print(("    " x (@lastpath + 1)) . "$path[@lastpath] {\n");
 	push @lastpath, $path[@lastpath];
+	if (@path == @lastpath) {
+	    for my $propval (@{$fdt{$node}}) {
+		my ($prop, $val) = @$propval;
+		if (defined $val) {
+		    print(("    " x (@lastpath + 1)) . "$prop = $val;\n");
+		} else {
+		    print(("    " x (@lastpath + 1)) . "$prop;\n");
+		}
+	    }
+	}
     }
-    for my $prop (sort keys %{$fdt{$node}}) {
-	print "$prop = " . $fdt{$node}{$prop} . ";\n";
+    my @cnodes;
+    my @ncnodes;
+    for my $node2 (@fdt) {
+	if (substr($node2, 0, length($node)) eq $node) {
+	    push @cnodes, $node2;
+	} else {
+	    push @ncnodes, $node2;
+	}
     }
+    @fdt = (@cnodes, @ncnodes);
 }
 while (@lastpath) {
-    print "};\n";
     pop @lastpath;
+    print(("    " x (@lastpath + 1)) . "};\n");
 }
+
+print("};\n");
