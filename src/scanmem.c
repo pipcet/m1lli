1d072eda9143ec11f75f45714e15721c413640f5
Pip Cet <pipcet@gmail.com>
2021-05-13 23:00:11 +0000

    src/scanmem.c: make multi-threaded, or attempt to

--- src/scanmem.c
+++ src/scanmem.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <sys/mman.h>
 #include <sys/fcntl.h>
 #include <stdio.h>
@@ -6,13 +8,112 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <pthread.h>
+
+#define ARRAYELTS(x) ((sizeof(x)/sizeof((x)[0])))
+static int fd;
+static pid_t child;
+static int pfd[4];
+static char *command;
+
+static void *thread_routine(void *offset_v)
+{
+  unsigned long off = (unsigned long)offset_v;
+  volatile unsigned long *page = mmap(NULL, 16384, PROT_READ|PROT_WRITE,
+				      MAP_SHARED, fd, off);
+
+  if (page == MAP_FAILED)
+    return NULL;
+
+  /* just our marker constant. */
+  while (page[0] == 0x5a6b448a98b350b6) {
+    /* Make sure that the page counter is still being incremented
+     * (i.e. there's live code on the other side). */
+    unsigned long count = page[1];
+    struct timeval starttime, curtime;
+    gettimeofday(&starttime, NULL);
+    while (page[1] == count) {
+      gettimeofday(&curtime, NULL);
+      if (1000000L * (curtime.tv_sec - starttime.tv_sec) + (curtime.tv_usec - starttime.tv_usec) > 100000)
+	goto noluck;
+    }
+
+    switch(page[4]) {
+    case 'R': {
+      /* read half a page */
+      unsigned long addr = page[3];
+      volatile void *mapped = mmap(NULL, 16384, PROT_READ|PROT_WRITE,
+				   MAP_SHARED, fd, addr & ~16383L);
+      memset((void *)(page + 1024), 0, 8192);
+      memcpy((void *)(page + 1024), (void *)(mapped + (addr & 16383)), 8192);
+      break;
+    }
+    case 'W': {
+      /* write half a page */
+      unsigned long addr = page[3];
+      volatile void *mapped = mmap(NULL, 8192, PROT_READ|PROT_WRITE,
+				   MAP_SHARED, fd, addr);
+      memset((void *)(page + 1024), 0, 8192);
+      memcpy((void *)(mapped), (void *)(page + 1024), 8192);
+      break;
+    }
+    case 'C': {
+      /* execute an immediate command, returning up to 8192 bytes of data */
+      unsigned long addr = page[3];
+      FILE *stream = popen((char *)(page + 1024), "r");
+      page[3] = fread((void *)(page + 1024), 1, 8192, stream);
+      pclose(stream);
+      break;
+    }
+    case '|': {
+      /* execute a command with stdio */
+      command = strdup((char *)(page + 1024));
+      child = fork();
+      pipe(pfd);
+      pipe(pfd + 2);
+      if (child == 0) {
+	close(pfd[1]);
+	close(pfd[2]);
+	dup2(pfd[0], 0);
+	dup2(pfd[3], 1);
+	dup2(pfd[3], 2);
+	system(command);
+	exit(0);
+      } else {
+	close(pfd[0]);
+	close(pfd[3]);
+      }
+      break;
+    }
+    case '>': {
+      /* send to stdio command */
+      page[3] = write(pfd[1], (void *)(page + 1024), page[3]);
+      break;
+    }
+    case '<': {
+      /* receive from stdio command */
+      fcntl(pfd[2], F_SETFL, O_NONBLOCK);
+      page[3] = read(pfd[2], (void *)(page + 1024), 8192);
+      break;
+    }
+    default:
+      break;
+    }
+    page[4] = 0;
+  }
+ noluck:
+  munmap((void *)page, 16384);
+  return NULL;
+}
+
+static struct {
+  pthread_t thread;
+  unsigned long off;
+} threads[16];
 
 int main(void)
 {
-  int fd = open("/dev/mem", O_RDWR);
-  pid_t child;
-  int pfd[4];
-  char *command;
+  fd = open("/dev/mem", O_RDWR);
   while (true) {
     /* This is important, or we'll hog the memory bus and MacOS won't boot. */
     sleep(2);
@@ -23,85 +124,31 @@ int main(void)
 	if (page == MAP_FAILED)
 	  continue;
 
-	/* just our marker constant. */
-	while (page[0] == 0x5a6b448a98b350b6) {
-	  /* Make sure that the page counter is still being incremented
-	   * (i.e. there's live code on the other side). */
-	  unsigned long count = page[1];
-	  struct timeval starttime, curtime;
-	  gettimeofday(&starttime, NULL);
-	  while (page[1] == count) {
-	    gettimeofday(&curtime, NULL);
-	    if (1000000L * (curtime.tv_sec - starttime.tv_sec) + (curtime.tv_usec - starttime.tv_usec) > 100000)
-	      goto noluck;
-	  }
-
-	  switch(page[4]) {
-	  case 'R': {
-	    /* read half a page */
-	    unsigned long addr = page[3];
-	    volatile void *mapped = mmap(NULL, 16384, PROT_READ|PROT_WRITE,
-					 MAP_SHARED, fd, addr & ~16383L);
-	    memset((void *)(page + 1024), 0, 8192);
-	    memcpy((void *)(page + 1024), (void *)(mapped + (addr & 16383)), 8192);
-	    page[4] = 0;
-	    break;
-	  }
-	  case 'W': {
-	    /* write half a page */
-	    unsigned long addr = page[3];
-	    volatile void *mapped = mmap(NULL, 8192, PROT_READ|PROT_WRITE,
-					 MAP_SHARED, fd, addr);
-	    memset((void *)(page + 1024), 0, 8192);
-	    memcpy((void *)(mapped), (void *)(page + 1024), 8192);
-	    page[4] = 0;
-	    break;
-	  }
-	  case 'C': {
-	    /* execute an immediate command, returning up to 8192 bytes of data */
-	    unsigned long addr = page[3];
-	    FILE *stream = popen((char *)(page + 1024), "r");
-	    page[3] = fread((void *)(page + 1024), 1, 8192, stream);
-	    pclose(stream);
-	    page[4] = 0;
-	    break;
-	  }
-	  case '|': {
-	    /* execute a command with stdio */
-	    command = strdup((char *)(page + 1024));
-	    child = fork();
-	    pipe(pfd);
-	    pipe(pfd + 2);
-	    if (child == 0) {
-	      close(pfd[1]);
-	      close(pfd[2]);
-	      dup2(pfd[0], 0);
-	      dup2(pfd[3], 1);
-	      system(command);
-	      exit(0);
-	    } else {
-	      close(pfd[0]);
-	      close(pfd[3]);
+	if (page[0] == 0x5a6b448a98b350b6) {
+	  size_t i;
+	  for (i = 0; i < ARRAYELTS(threads); i++) {
+	    if (threads[i].off == 0) {
+	    } else if (pthread_tryjoin_np(threads[i].thread, NULL) == 0) {
+	      threads[i].off = 0;
+	    } else if (threads[i].off == off) {
+	      goto nextoff;
 	    }
-	    break;
 	  }
-	  case '>': {
-	    /* send to stdio command */
-	    page[3] = write(pfd[1], (void *)(page + 1024), page[3]);
-	    break;
+	  for (i = 0; i < ARRAYELTS(threads); i++) {
+	    if (threads[i].off == 0) {
+	      if (pthread_create(&threads[i].thread, NULL, thread_routine,
+				 (void *)off) != 0) {
+		thread_routine((void *)off);
+	      } else {
+		threads[i].off = off;
+	      }
+	      goto nextoff;
+	    }
 	  }
-	  case '<': {
-	    /* receive from stdio command */
-	    fcntl(pfd[2], F_SETFL, O_NONBLOCK);
-	    page[3] = read(pfd[2], (void *)(page + 1024), page[3]);
-	    break;
-	  }
-	  default:
-	    break;
-	  }
+	  thread_routine((void *)off);
 	}
-	noluck:
-	  munmap((void *)page, 16384);
+      nextoff:
+	munmap((void *)page, 16384);
       }
   }
 
