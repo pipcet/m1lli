fc8e2c253d30b76d9025f9a4fa650d3269ae4380
Pip Cet <pipcet@gmail.com>
2021-05-13 19:31:40 +0000

    src/scanmem.c: support the minimal mmio tracer

--- src/scanmem.c
+++ src/scanmem.c
@@ -12,7 +12,7 @@ int main(void)
   pid_t child;
   int pfd[4];
   char *command;
-  for (unsigned long off = 0x800000000; off < 0x900000000; off += 16384)
+  for (unsigned long off = 0x800000000; off < 0x980000000; off += 16384)
     {
       volatile unsigned long *page = mmap(NULL, 16384, PROT_READ|PROT_WRITE,
 				 MAP_SHARED, fd, off);
@@ -20,10 +20,13 @@ int main(void)
 	continue;
 
     again:
+      /* shared random constant */
       if (page[0] != 0x5a6b448a98b350b6) {
 	goto noluck;
       }
 
+      /* Make sure that the page counter is still being incremented
+       * (i.e. there's live code on the other side). */
       unsigned long count = page[1];
       struct timeval starttime, curtime;
       gettimeofday(&starttime, NULL);
@@ -35,6 +38,7 @@ int main(void)
 
       switch(page[4]) {
       case 'R': {
+	/* read half a page */
 	unsigned long addr = page[3];
 	volatile void *mapped = mmap(NULL, 16384, PROT_READ|PROT_WRITE,
 				     MAP_SHARED, fd, addr & ~16383L);
@@ -44,6 +48,7 @@ int main(void)
 	goto again;
       }
       case 'W': {
+	/* write half a page */
 	unsigned long addr = page[3];
 	volatile void *mapped = mmap(NULL, 8192, PROT_READ|PROT_WRITE,
 				     MAP_SHARED, fd, addr);
@@ -53,6 +58,7 @@ int main(void)
 	goto again;
       }
       case 'C': {
+	/* execute an immediate command, returning up to 8192 bytes of data */
 	unsigned long addr = page[3];
 	FILE *stream = popen((char *)(page + 1024), "r");
 	page[3] = fread((void *)(page + 1024), 1, 8192, stream);
@@ -61,28 +67,31 @@ int main(void)
 	goto again;
       }
       case '|': {
+	/* execute a command with stdio */
 	command = strdup((char *)(page + 1024));
 	child = fork();
 	pipe(pfd);
 	pipe(pfd + 2);
 	if (child == 0) {
-	  close(pfd + 1);
-	  close(pfd + 2);
-	  dup2(pfd, 0);
-	  dup2(pfd + 3, 1);
+	  close(pfd[1]);
+	  close(pfd[2]);
+	  dup2(pfd[0], 0);
+	  dup2(pfd[3], 1);
 	  system(command);
 	  exit(0);
 	} else {
-	  close(pfd);
-	  close(pfd + 3);
+	  close(pfd[0]);
+	  close(pfd[3]);
 	}
 	goto again;
       }
       case '>': {
+	/* send to stdio command */
 	page[3] = write(pfd[1], (void *)(page + 1024), page[3]);
 	goto again;
       }
       case '<': {
+	/* receive from stdio command */
 	page[3] = read(pfd[2], (void *)(page + 1024), page[3]);
 	goto again;
       }
