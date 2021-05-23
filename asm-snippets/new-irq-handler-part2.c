2b267ed0952d65e2e562ddafc2eb68744b2027d6
Pip Cet <pipcet@gmail.com>
2021-05-23 13:04:10 +0000

    asm-snippets/new-irq-handler-part2.c: snapshot

--- asm-snippets/new-irq-handler-part2.c
+++ asm-snippets/new-irq-handler-part2.c
@@ -8,19 +8,22 @@ long mmiotrace(unsigned long frame)
   *(VPAGE + (0x3fa0/8)) = *(unsigned long *)(frame + 32 * 8);
   *(VPAGE + (0x3fa8/8)) = *(unsigned long *)(frame + 33 * 8);
   unsigned long esr;
-  asm volatile("mrs %0, esr_el1" : "=r" (esr));
   //if ((esr & 0xe8000000) != 0x80000000)
   //  return 0;
   unsigned long far;
-  asm volatile("mrs %0, far_el2" : "=r" (far));
   unsigned long elr;
-  asm volatile("mrs %0, elr_el2" : "=r" (elr));
   unsigned long ttbr0;
   unsigned long ttbr1;
   if (off >= 0x13ec000) {
+    asm volatile("mrs %0, s3_6_c15_c10_5" : "=r" (esr));
+    asm volatile("mrs %0, s3_6_c15_c10_7" : "=r" (far));
+    asm volatile("mrs %0, s3_6_c15_c10_6" : "=r" (elr));
     asm volatile("mrs %0, ttbr0_el2" : "=r" (ttbr0));
     asm volatile("mrs %0, ttbr1_el2" : "=r" (ttbr1));
   } else {
+    asm volatile("mrs %0, esr_el1" : "=r" (esr));
+    asm volatile("mrs %0, far_el2" : "=r" (far));
+    asm volatile("mrs %0, elr_el2" : "=r" (elr));
     asm volatile("mrs %0, ttbr0_el1" : "=r" (ttbr0));
     asm volatile("mrs %0, ttbr1_el1" : "=r" (ttbr1));
   }
@@ -29,8 +32,8 @@ long mmiotrace(unsigned long frame)
   *(VPAGE + (0x3fd8/8)) = esr;
   *(VPAGE + (0x3fe0/8)) = ttbr0;
   *(VPAGE + (0x3fe8/8)) = ttbr1;
- again:
   *(VPAGE + (0x3ff8/8)) = elr;
+  asm volatile("mrs %0, elr_el2" : "=r" (*(VPAGE + (0x3e10/8))));
   asm volatile("dmb sy" : : : "memory");
   asm volatile("dsb sy");
   asm volatile("isb");
@@ -47,10 +50,21 @@ long mmiotrace(unsigned long frame)
   asm volatile("dsb sy");
   asm volatile("isb");
   elr = *(VPAGE + (0x3ff8/8));
- skip:
-  asm volatile("msr elr_el2, %0" : : "r" (elr));
+  if (off >= 0x13ec000) {
+    volatile unsigned x = 0x10000000;
+    while(--x);
+    //asm volatile("msr elr_el2, %0" : : "r" (elr));
+    asm volatile("msr s3_6_c15_c10_6, %0" : : "r" (elr));
+    unsigned long tmp;
+    asm volatile("mrs %0, s3_6_c15_c10_4" : "=r" (tmp));
+    tmp &= ~1L;
+    asm volatile("msr s3_6_c15_c10_4, %0" : : "r" (tmp));
+    asm volatile("msr s3_6_c15_c10_6, %0" : : "r" (elr));
+  } else {
+    asm volatile("msr elr_el2, %0" : : "r" (elr));
+  }
 
-  if (*(VPAGE + (0x3fb0/8)) != 0) {
+  if (0) if (*(VPAGE + (0x3fb0/8)) != 0) {
     asm volatile("dmb sy" : : : "memory");
     asm volatile("dsb sy");
     asm volatile("isb");
@@ -83,5 +97,5 @@ long mmiotrace(unsigned long frame)
   long ret = (*(VPAGE + (0x3fd0/8))) != 0;
   asm volatile("tlbi alle1");
   asm volatile("tlbi alle2");
-  return ret;
+  return ret ? ((off >= 0x13ec000) ? 2 : 1) : 0;
 }
